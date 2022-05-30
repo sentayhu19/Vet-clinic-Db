4e7bf428095516c17b7c23e128810265fe282d28
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-05-30 16:27:02 +0300

    ID Autoincremente and Not null prop added

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 CREATE TABLE animals (
-id INT PRIMARY KEY,
+id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
 name VARCHAR(50) NOT NULL ,
 date_of_birth DATE NOT NULL ,
 escape_attempts INT NOT NULL,
